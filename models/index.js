33430922b42c3f1bb4ad23c35cfd6949da5d88db
WrittenInnaCode <writteninnacode@gmail.com>
2022-10-09 22:04:20 -0700

    created server.js and made corrections in models/index.js

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('.Comment');
+const Comment = require('./Comment');
 
+//create associations
 User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
 });
 
 Post.belongsTo(User, {
@@ -20,6 +25,10 @@ Post.hasMany(Comment, {
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
 });
 
 
